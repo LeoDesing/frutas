09a2abde64bae4b69b8afa4b147bee7b8e46de18
LeoDesing <95183849+LeoDesing@users.noreply.github.com>
2021-12-07 20:34:17 -0500

    Add files via upload
    Practica diseño web frutas

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let boton = document.querySelector("#menu");
+let mobile = document.querySelector("#mobile");
+
+mobile.addEventListener("click", function(){
+   boton.classList.toggle("toggle-menu");
+// alert("hola");
+});
